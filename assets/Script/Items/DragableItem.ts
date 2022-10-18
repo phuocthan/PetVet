7f78610330116edf7d6071ed5036be581bd8f964
Nhat Nguyen <nhat.nguyen-long@ubisoft.com>
2022-10-18 21:42:32 +0700

    Update dragable item

--- assets/Script/Items/DragableItem.ts
+++ assets/Script/Items/DragableItem.ts
@@ -10,26 +10,57 @@ export enum DragableItemState {
 
 @ccclass
 export default class DragableItem extends cc.Component {
+    /**
+     * Property Declaration
+     */
     @property()
     itemType: string = "";
 
-    public currentState: DragableItemState = DragableItemState.Inactive;
+    public _currentState: DragableItemState = DragableItemState.Inactive;
+
+    protected _originalPosition: cc.Vec3 = null;
+    /**
+     * On Load
+     */
+    onLoad() {
+        this.initOriginalPosition();
+    }
     /**
      * Start
      */
     start() {
-        this.node.on(cc.Node.EventType.TOUCH_START, (touch, event) => {
-            // return the touch point with world coordinates
-            
+        this.node.on(cc.Node.EventType.TOUCH_START, (touch: cc.Event.EventTouch) => {
+            this.setState(DragableItemState.Holding);
         }, this);
-        this.node.on(cc.Node.EventType.TOUCH_MOVE, (touch, event) => {
-            // return the touch point with world coordinates
-            
+        this.node.on(cc.Node.EventType.TOUCH_MOVE, (touch: cc.Event.EventTouch) => {
+            const position = this.node.position.clone();
+            position.x += touch.getDelta().x;
+            position.y += touch.getDelta().y;
+            this.node.setPosition(position);
         }, this);
-        this.node.on(cc.Node.EventType.TOUCH_END, (touch, event) => {
-            // return the touch point with world coordinates
-            
+        this.node.on(cc.Node.EventType.TOUCH_END, (touch: cc.Event.EventTouch) => {
+            this.setState(DragableItemState.Idle);
+            this.restoreOriginalPosition();
         }, this);
+    }
+    /**
+     * Init item original position
+     */
+    initOriginalPosition() {
+        this._originalPosition = this.node.position.clone();
+    }
+    /**
+     * Restore to original position
+     */
+    restoreOriginalPosition() {
+        this.node.setPosition(this._originalPosition);
+    }
+    /**
+     * Set state of current dragable item
+     * @param state 
+     */
+    setState(state: DragableItemState) {
+        this._currentState = state;
     }
     /**
      * Update
